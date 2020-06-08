47abf04695742a525c93399cfaa0dde75e38a770
Evgeniya Chernaya <evgenya.chernaya@gmail.com>
2020-06-08 10:35:26 -0700

    add functionality to the scripts.js to show inputs, styles to styles.css
    co-authored-by: Taylor Phillips <taylorphillips133@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,13 @@
 $(document).ready(function(){
-  
-
+  $("form#input").submit(function(event){
+    let inputArray=[];
+    inputArray.push($("#color").val(), $("#season").val(), $("#animal").val(), $("#food").val());
+    let newArray=[];
+    newArray.push(inputArray[1], inputArray[0], inputArray[2]);
+    $("li#first").text(newArray[0]);
+    $("li#second").text(newArray[1]);
+    $("li#third").text(newArray[2]);
+    $(".result").show();
+    event.preventDefault();
+  });
 });
